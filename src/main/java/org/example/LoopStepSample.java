2133a8d1c4542eb0a5d4a3601acaf70cf8a73e44
ats-3fx <atsushi.kawano@techpark.ed.jp>
2022-10-14 18:51:29 +0900

    p.178

--- src/main/java/org/example/LoopStepSample.java
+++ src/main/java/org/example/LoopStepSample.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class LoopStepSample {
     public static void main(String[] args) {
-        for(int i = 1; i <= 5; i++){
+        for(int i = 5; i >= 1; i--){
             System.out.print("0".repeat(i));
             System.out.println();
             }
