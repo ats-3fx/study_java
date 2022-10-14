336186230ee8656fec4e6d46fa459cd7190c7e20
ats-3fx <atsushi.kawano@techpark.ed.jp>
2022-10-14 19:04:58 +0900

    p.179 丸を並べる

--- src/main/java/org/example/LoopStepSample.java
+++ src/main/java/org/example/LoopStepSample.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class LoopStepSample {
     public static void main(String[] args) {
-        for(int i = 5; i >= 1; i--){
-            System.out.print("0".repeat(i));
+        for(int i = 1; i <= 5; i++){
+            System.out.print("0".repeat(6-i));
             System.out.println();
             }
         }
